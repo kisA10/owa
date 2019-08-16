26f1b63d918e224fb34d562048d4c55c5ec61afa
kisA10 <kis@uxvvantage.com>
2019-08-16 16:44:10 +0900

    fps30 OK

--- as_cv_test.py
+++ as_cv_test.py
@@ -4,7 +4,7 @@ BlueRov video capture class modified by ak
 """
 import sys
 import cv2
-#import gi
+import gi
 import numpy as np
 import time
 
@@ -13,8 +13,9 @@ import socket
 from contextlib import closing
 import threading
 
-#gi.require_version('Gst', '1.0')
-#from gi.repository import Gst
+
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
 
 log = logging.getLogger()
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
@@ -39,8 +40,137 @@ class Command_input(threading.Thread):
         while True:
             self.command = self.sock.recv(self.bufsize).decode()
             log.info(self.command)
-            time.sleep(0.01)
+            time.sleep(0.1)
 
+class Video():
+    """BlueRov video capture class constructor
+
+    Attributes:
+        port (int): Video UDP port
+        video_codec (string): Source h264 parser
+        video_decode (string): Transform YUV (12bits) to BGR (24bits)
+        video_pipe (object): GStreamer top-level pipeline
+        video_sink (object): Gstreamer sink element
+        video_sink_conf (string): Sink configuration
+        video_source (string): Udp source ip and port
+    """
+
+    def __init__(self, port=5700):
+        """Summary
+
+        Args:
+            port (int, optional): UDP port
+        """
+
+        Gst.init(None)
+
+        self.port = port
+        self._frame = None
+
+        # [Software component diagram](https://www.ardusub.com/software/components.html)
+        # UDP video stream (:5600)
+        self.video_source = 'udpsrc port={}'.format(self.port)
+        # [Rasp raw image](http://picamera.readthedocs.io/en/release-0.7/recipes2.html#raw-image-capture-yuv-format)
+        # Cam -> CSI-2 -> H264 Raw (YUV 4-4-4 (12bits) I420)
+        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
+        # Python don't have nibble, convert YUV nibbles (4-4-4) to OpenCV standard BGR bytes (8-8-8)
+        self.video_decode = \
+            '! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert'
+        # Create a sink to get data
+        self.video_sink_conf = \
+            '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
+
+        self.video_pipe = None
+        self.video_sink = None
+
+        self.run()
+
+    def start_gst(self, config=None):
+        """ Start gstreamer pipeline and sink
+        Pipeline description list e.g:
+            [
+                'videotestsrc ! decodebin', \
+                '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
+                '! appsink'
+            ]
+
+        Args:
+            config (list, optional): Gstreamer pileline description list
+        """
+
+        if not config:
+            config = \
+                [
+                    'videotestsrc ! decodebin',
+                    '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
+                    '! appsink'
+                ]
+
+        command = ' '.join(config)
+        self.video_pipe = Gst.parse_launch(command)
+        self.video_pipe.set_state(Gst.State.PLAYING)
+        self.video_sink = self.video_pipe.get_by_name('appsink0')
+
+    @staticmethod
+    def gst_to_opencv(sample):
+        """Transform byte array into np array
+
+        Args:
+            sample (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
+        buf = sample.get_buffer()
+        caps = sample.get_caps()
+        array = np.ndarray(
+            (
+                caps.get_structure(0).get_value('height'),
+                caps.get_structure(0).get_value('width'),
+                3
+            ),
+            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8)
+        return array
+
+    def frame(self):
+        """ Get Frame
+
+        Returns:
+            iterable: bool and image frame, cap.read() output
+        """
+        return self._frame
+
+    def frame_available(self):
+        """Check if frame is available
+
+        Returns:
+            bool: true if frame is available
+        """
+        return type(self._frame) != type(None)
+
+    def run(self):
+        """ Get frame to update _frame
+        """
+
+        self.start_gst(
+            [
+                self.video_source,
+                self.video_codec,
+                self.video_decode,
+                self.video_sink_conf
+            ])
+
+        self.video_sink.connect('new-sample', self.callback)
+
+    def callback(self, sink):
+        sample = sink.emit('pull-sample')
+        new_frame = self.gst_to_opencv(sample)
+        self._frame = new_frame
+
+        return Gst.FlowReturn.OK
+
+
+        
 
 if __name__ == '__main__':
     # Create the video object
@@ -49,40 +179,76 @@ if __name__ == '__main__':
     argvs = sys.argv  # コマンドライン引数を格納したリストの取得
     # argc = len(argvs) # 引数の個数
     # argvs[1]にportを入れて呼び出す
-    #port = 5603#argvs[1]
+    
+    udp_port = int(argvs[1])
     cam_name = argvs[2]
-    command_port = int(argvs[3])
+    com_port = int(argvs[3])
+    frame_x =  int(argvs[4])
+    frame_y =  int(argvs[5])
 
-    #video1 = Video(5603)
-    #video1.start()
+    rot_c_x = float(argvs[6])
+    rot_c_y = float(argvs[7])
+    rot_ang = float(argvs[8])
+    scale = float(argvs[9])
+
+    
+
+
+
+    
+    video = Video(udp_port)
+    
 
  
 
-    command_input = Command_input(command_port)
+    command_input = Command_input(com_port)
     command_input.start()
+
+    framesize = "resize"
 
 
 
     while True:
         # Wait for the next frame
-        #if not video1.frame_available():
-        #    continue
-        time.sleep(0.001)
+        if not video.frame_available():
+            continue
+        
+        
+
+        frame = video.frame()
+        frame_h = frame.shape[0]
+        frame_w = frame.shape[1]
+        rot_center = ( int(frame_w*(1+rot_c_x/100)/2) , int(frame_h*(1+rot_c_y/100)/2))
+
+        trans = cv2.getRotationMatrix2D(rot_center, rot_ang , scale)
+        frame = cv2.warpAffine(frame, trans, (frame_w,frame_h))
+
+        resized_frame = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),(int(frame_w/3), int(frame_h/3)))
+        
+        if framesize == "normal":
+            cv2.imshow(cam_name, frame)
+        elif framesize == "resize":
+            resized_frame = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),(int(frame_w/3), int(frame_h/3)))
+            cv2.imshow(cam_name, resized_frame)
+            cv2.moveWindow(cam_name, frame_x,frame_y)
 
         if command_input.command == "g":
             print(cam_name + " command g incomming")
+            #command clear
             command_input.command = ""
+            cv2.imwrite(cam_name+'frame.png', frame)
             #break
         
-        #print(command_input.command)
+        elif command_input.command == "fr":
+            command_input.command = ""
+            framesize = "resize"
+        
+        elif command_input.command == "fn":
+            command_input.command = ""
+            framesize = "normal"
 
-        #frame1 = video1.frame()
-        #cv2.imshow(cam_name, frame1)
+        
+        #time.sleep(0.01)
 
-        #frame2 = video2.frame()
-        #cv2.imshow(cam_name, frame2)
-
-
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-         #   command_input.stop()
-         #   break
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+           break
--- chain_command.py
+++ chain_command.py
@@ -25,13 +25,38 @@ sock3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port4 = '5706'
 sock4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+x0='0'
+x1='640'
+y0='0'
+y1='480'
+
+#calibration value
+cam1_rot_c_dx = '2.3' #%
+cam1_rot_c_dy = '1' #%
+cam1_rot_ang = '30'
+cam1_scale = '0.5'
+
+cam2_rot_c_dx = '0.5' #%
+cam2_rot_c_dy = '-10' #%
+cam2_rot_ang = '-5'
+cam2_scale = '2'
+
+cam3_rot_c_dx = '40' #%
+cam3_rot_c_dy = '10' #%
+cam3_rot_ang = '0.5'
+cam3_scale = '0.8'
+
+cam4_rot_c_dx = '20' #%
+cam4_rot_c_dy = '10' #%
+cam4_rot_ang = '10'
+cam4_scale = '1'
 
 
-pro1 = sb.Popen(['python3','as_cv_test.py','5603','CAM1',port1],preexec_fn=os.setsid)
-pro2 = sb.Popen(['python3','as_cv_test.py','5604','CAM2',port2],preexec_fn=os.setsid)
-pro3 = sb.Popen(['python3','as_cv_test.py','5605','CAM3',port3],preexec_fn=os.setsid)
-pro4 = sb.Popen(['python3','as_cv_test.py','5606','CAM4',port4],preexec_fn=os.setsid)
 
+pro1 = sb.Popen(['python3','as_cv_test.py','5603','CAM1',port1,x0,y0,cam1_rot_c_dx,cam1_rot_c_dy,cam1_rot_ang,cam1_scale],preexec_fn=os.setsid)
+pro2 = sb.Popen(['python3','as_cv_test.py','5604','CAM2',port2,x1,y0,cam2_rot_c_dx,cam2_rot_c_dy,cam2_rot_ang,cam2_scale],preexec_fn=os.setsid)
+pro3 = sb.Popen(['python3','as_cv_test.py','5605','CAM3',port3,x0,y1,cam3_rot_c_dx,cam3_rot_c_dy,cam3_rot_ang,cam3_scale],preexec_fn=os.setsid)
+pro4 = sb.Popen(['python3','as_cv_test.py','5606','CAM4',port4,x1,y1,cam4_rot_c_dx,cam4_rot_c_dy,cam4_rot_ang,cam4_scale],preexec_fn=os.setsid)
 
 
 #ここからコマンドを送信する
@@ -48,15 +73,15 @@ while True:
         break
             
 
-    elif command == 'g':
-        print("send command g")
+    elif not command == '':
+        print("send command "+command)
         sock1.sendto(command.encode(), (host, int(port1)))
         sock2.sendto(command.encode(), (host, int(port2)))
         sock3.sendto(command.encode(), (host, int(port3)))
         sock4.sendto(command.encode(), (host, int(port4)))
 
 sock1.close()
+
 sock2.close()
 sock3.close()
-sock4.close()
-        
+sock4.close()   
